5b7ce4f421cb73d98c0d26fdda3ddbb0585669d2
jpedrozzv <joaopedrocamposamaral73@gmail.com>
2025-08-21 22:48:04 -0300

    atualização

--- src/main/java/com/programacao/web/fatec/api_fatec/controller/ClienteController.java
+++ src/main/java/com/programacao/web/fatec/api_fatec/controller/ClienteController.java
@@ -7,12 +7,15 @@ import com.programacao.web.fatec.api_fatec.entities.Cliente;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -33,6 +36,12 @@ public class ClienteController {
         listaDeCliente.add(cliente2);
     }
 
+    @GetMapping("/listarClientes")
+    public List<Cliente> listarClientes() {
+        return listaDeCliente;
+    }
+    
+
     @GetMapping("/testeCliente1") //-> /api/clientes/testeCliente1
     public String testeCliente() {
         return "Teste Client";
@@ -45,7 +54,36 @@ public class ClienteController {
 
     @PostMapping("")
     public Cliente createCliente(@RequestBody Cliente cliente) {
+
+        listaDeCliente.add(cliente);
+
         return cliente;
     }
+
+    @DeleteMapping("/{id}")
+    public String deletarCliente(@PathVariable Long id) {
+        
+        for (Cliente cliente: listaDeCliente) {
+                if (cliente.getId() == id) {
+                    listaDeCliente.remove(cliente);
+                    return "OK";
+                }
+        }
+
+        return "NÃO ENCONTRADO ID:"+id;
+    }
+
+    @PutMapping("/{id}")
+    public String alterarCliente(@PathVariable Long id, @RequestBody Cliente entity) {
+        
+         for (Cliente cliente: listaDeCliente) {
+                if (cliente.getId() == id) {
+                    cliente.setId(id);
+                    cliente.setNome(entity.getNome());
+                    return "ENCONTROU";
+                }
+        }
+        
+        return "NÃO ENCONTRADO ID:"+id;    }
     
 }
