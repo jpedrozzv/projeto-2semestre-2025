766ac713398a4e8c552fe8afd45a91f5d164bdfe
jpedrozzv <joaopedrocamposamaral73@gmail.com>
2025-08-21 22:18:39 -0300

    atualização

--- src/main/java/com/programacao/web/fatec/api_fatec/controller/ClienteController.java
+++ src/main/java/com/programacao/web/fatec/api_fatec/controller/ClienteController.java
@@ -1,25 +1,51 @@
 package com.programacao.web.fatec.api_fatec.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.programacao.web.fatec.api_fatec.entities.Cliente;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
+    
+    private final List<Cliente> listaDeCliente = new ArrayList<>();
 
+    public ClienteController() {
+        //forma1
+        listaDeCliente.add(new Cliente(1L, "Danilo"));
 
-    @GetMapping("/testeCliente1") // --> /api/cliente/testeCliente1
-    public String testeCliente(){
-        return "Teste Client";
-
+        //forma2
+        Cliente cliente2 = new Cliente();
+        cliente2.setId(2L);
+        cliente2.setNome("Joao");
+        listaDeCliente.add(cliente2);
     }
 
-    @GetMapping("/testeCliente2/{nome}") // --> /api/cliente/testeCliente2
-    public String testeCliente2(@PathVariable String nome){
-        return nome;
+    @GetMapping("/testeCliente1") //-> /api/clientes/testeCliente1
+    public String testeCliente() {
+        return "Teste Client";
+    }
 
+    @GetMapping("/testeCliente2/{nome}") //-> /api/clientes/testeCliente2/
+    public String testeCliente2(@PathVariable String nome) {
+        return nome;
+    }
+
+    @PostMapping("")
+    public Cliente createCliente(@RequestBody Cliente cliente) {
+        return cliente;
     }
     
 }
--- src/main/java/com/programacao/web/fatec/api_fatec/entities/cliente.java
+++ src/main/java/com/programacao/web/fatec/api_fatec/entities/cliente.java
@@ -1,26 +1,33 @@
 package com.programacao.web.fatec.api_fatec.entities;
 
 public class Cliente {
+    
+
     private Long id;
-   
+    
     private String nome;
- 
+
     public Cliente() {
-       
+        
     }
- 
+
+    public Cliente(Long cod, String nome) {
+        this.id = cod;
+        this.nome = nome;
+    }
+
     public Long getId() {
         return id;
     }
- 
+
     public void setId(Long id) {
         this.id = id;
     }
- 
+
     public String getNome() {
         return nome;
     }
- 
+
     public void setNome(String nome) {
         this.nome = nome;
     }
